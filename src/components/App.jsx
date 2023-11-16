475488f0d759cd9cfcf2a73877f71772a802bf1e
SSSerg05 <ssserg05@gmail.com>
2023-11-16 08:58:31 +0200

    add Task1-Task7

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,45 @@
+import React, { useState } from "react";
 import "./App.css"
 
 export const App = () => {
+  const [list, setList] = useState([]);
+  const ulRef = React.createRef();
+  const input2Ref = React.createRef();
+
+  const h1 = React.createElement('h1',{},'Hi world!');
+  const h2 = React.createElement('h2',{className: 'text', style:{color: 'orange'}},'header 2');
+  const p = React.createElement('p',{style:{color: 'red'}},'this is p');
+  const input = React.createElement('input', {defaultValue:'55'});
+  const p1 = React.createElement('p',{},'hi');
+  const p2 = React.createElement('p',{},'world');
+  const div = React.createElement('div',{className: 'text-grey', style:{color: 'grey'}}, p1, p2);
+
+  const handleClick = (e) => {
+    const val = input2Ref.current.value.trim();
+    if (!val) {
+      return
+    }
+    
+    const data = new Date();
+    const li = React.createElement('li', {key: data}, val);
+    setList([...list, li]);
+    input2Ref.current.value = '';
+  }
+  const input2 = React.createElement('input', {ref:input2Ref});
+  const button = React.createElement('button', {type:'button', onClick: handleClick}, 'Add item to list');
+
   return (
     <div>
-
+      {h1}
+      {h2}
+      {p}
+      {input}
+      {div}
+      {input2}
+      {button}
+      <ul ref={ulRef}>
+        {list.map(item => item)}
+      </ul>
     </div>
   );
 };
